306927f6f797187f114588dde6a6e1fc7577330f
Richard Domander <rdomander@rvc.ac.uk>
2018-01-19 13:21:40 +0000

    Change source file licences

--- src/main/java/sc/fiji/pQCT/selectroi/liveWireEngine/LiveWireCosts.java
+++ src/main/java/sc/fiji/pQCT/selectroi/liveWireEngine/LiveWireCosts.java
@@ -1,3 +1,30 @@
+/*
+BSD 2-Clause License
+
+Copyright (c) 2018, Timo Rantalainen
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 package sc.fiji.pQCT.selectroi.liveWireEngine;
 
@@ -6,13 +33,13 @@ import java.util.PriorityQueue;
 import java.util.stream.DoubleStream;
 
 /**
- * Modified by Timo Rantalainen 2012 - 2014 tjrantal at gmail dot com from
- * IvusSnakes (http://ivussnakes.sourceforge.net/) ImageJ plugin A Class to
- * calculate LiveWire paths.
+ * Modified by Timo Rantalainen 2012 - 2014 from IvusSnakes
+ * (http://ivussnakes.sourceforge.net/) ImageJ plugin A Class to calculate
+ * LiveWire paths.
  * <p>
- * Changed the implementation back to the one suggested in Barret & Mortensen
- * 1997. Interactive live-wire boundary extraction. Medical Image Analysis
- * (1996/7) volume 1, number 4, pp 331-341.
+ * Changed the implementation back to the one suggested in Barret &amp;
+ * Mortensen 1997. Interactive live-wire boundary extraction. Medical Image
+ * Analysis (1996/7) volume 1, number 4, pp 331-341.
  * </p>
  */
 public class LiveWireCosts implements Runnable {
